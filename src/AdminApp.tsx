54ecb961d045efe6364ce513b74de3dd42625764
Jay-physics <gsolutionsjay@gmail.com>
2026-02-02 23:45:13 -0500

    Fix: Remove unused React import in AdminApp.tsx

--- src/AdminApp.tsx
+++ src/AdminApp.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import AdminLayout from './components/admin/AdminLayout'
 import Dashboard from './pages/admin/Dashboard'
